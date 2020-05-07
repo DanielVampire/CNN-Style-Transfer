9e7cf1ef9fccc8c119fe7b7b5a8a93b30bb910e8
DanielVampire <deadvampire1998@gmail.com>
2020-05-07 23:03:04 +0300

    Сделал вывод и удаление изображений
    Наконец то удалось, господи, 3 дня ебался с дичью
    Осталось припихнуть кнопку для вывода изображений, настроить кнопку "выбрать" и по функционалу диалог этот готов

--- CNN Style Transfer/Dialog.py
+++ CNN Style Transfer/Dialog.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5.uic as uic
+import functools
 
 dialogForm = uic.loadUiType("UI_Dialog.ui")[0]
 
@@ -9,56 +10,118 @@ class Dialog(QtWidgets.QDialog, dialogForm):
         QtWidgets.QDialog.__init__(self,None)
         self.setupUi(self)
         self.Images=[]
-        self.Styles=[]
-        self.Labels=[]
-        self.CountStyleinImages=[]
         self.TakePic.clicked.connect(self.TakePicture)
         self.TakeStyle.clicked.connect(self.TakeStyleImages)
         self.AddPic.clicked.connect(self.AddPictures)
+        self.TakeStyle.setEnabled(False)
+        self.labstyle.setVisible(False)
+        self.labizo.setVisible(False)
+        self.ImageCounter = 0
     def resizeEvent(self,event):
         event.accept()
     def TakePicture(self):
-        self.Images.append(QtWidgets.QFileDialog.getOpenFileName(None,"Укажите изображение",QtCore.QDir.homePath(),"Файл изображения (*.jpg);;Все файлы (*.*)"))
-        label = GoodQLabel(self)
-        label.clicked.connect(self.EventForLable)
-        pix=QtGui.QPixmap(self.Images[len(self.Images)-1][0])
-        label.setPixmap(pix.scaled(140,140))
-        self.ImageLayout.addWidget(label)
-        self.Labels.append(label)
+        t= list(QtWidgets.QFileDialog.getOpenFileName(None,"Укажите изображение",QtCore.QDir.homePath(),"Файл изображения (*.jpg);;Все файлы (*.*)"))
+        t.pop(1)
+        self.Images.append(t)
+        self.TakeStyle.setEnabled(True)
         self.TakePic.setEnabled(False)
     def TakeStyleImages(self):
-        self.Styles.append(QtWidgets.QFileDialog.getOpenFileName(None,"Укажите изображение стиль",QtCore.QDir.homePath(),"Файл изображения (*.jpg);;Все файлы (*.*)"))
-        label = GoodQLabel(self)
-        self.ImageLayout.addWidget(label)
-        label.clicked.connect(self.EventForLable)
-        pix=QtGui.QPixmap(self.Styles[len(self.Styles)-1][0])
-        label.setPixmap(pix.scaled(140,140))
-        emptyPix = QtGui.QPixmap(140,140)
-        emptyPix.fill(QtCore.Qt.white)
-        baglabel = QtWidgets.QLabel(self)
-        baglabel.setPixmap(emptyPix)
-
-        self.StyleLayout.addWidget(label)
-        if self.StyleLayout.count() > self.ImageLayout.count():
-            self.ImageLayout.addWidget(baglabel)
-        self.Labels.append(label)
-    
-    def EventForLable(self,event):
-        sender = self.sender()
-        for i in self.Labels:
-            if sender == i:
-                i.parent=None
-                i.deleteLater()
-                self.update()
+        for i in self.Images:
+            if (len(self.Images)-1) == self.Images.index(i):
+                t= list(QtWidgets.QFileDialog.getOpenFileName(None,"Укажите изображение стиль",QtCore.QDir.homePath(),"Файл изображения (*.jpg);;Все файлы (*.*)"))
+                t.pop(1)
+                t=str(t[0])
+                i.append(t)
+                break
+    def ShowImages(self):
+        Hlayout = QtWidgets.QHBoxLayout(self)
+        self.labstyle.setVisible(True)
+        self.labizo.setVisible(True)
+        Button = QtWidgets.QPushButton(self)
+        Button.setText("X")
+        Button.setMinimumSize(30,140)
+        Button.clicked.connect(lambda checked : self.DeleteImages( checked, Hlayout) )
+        for i in range(self.ImageCounter, len(self.Images)):
+            for j in self.Images[i]:
+                Image = QtWidgets.QLabel(self)
+                pix = QtGui.QPixmap(j)
+                Image.setPixmap(pix.scaled(140,140))
+                Hlayout.addWidget(Image)
+                per = self.Images[i]
+                if per.index(j) == 0:
+                    text = QtWidgets.QLabel(j)
+                    text.setVisible(False)
+                    Hlayout.addWidget(text)
+        self.ImageCounter+=1
+        Hlayout.addWidget(Button)
+        self.ImageShow.addLayout(Hlayout)
+    def DeleteImages(self,checked,Layout):
+        for i in range(self.ImageShow.count()):
+            obj = self.ImageShow.itemAt(i)
+            if obj == Layout:
+                rest = True
+                while rest:
+                    if Layout.count() == 0:
+                        break
+                    for iterate in range(Layout.count()):
+                        widget = Layout.itemAt(iterate).widget()
+                        if widget is None:
+                            rest=False
+                            break
+                        for image in self.Images:
+                            if widget.text() == image[0]:
+                                self.Images.remove(image)
+                                break
+                        Layout.removeWidget(widget)
+                        widget.deleteLater()
+                        widget = None
+                        break
+                self.ImageShow.removeItem(obj)
+                self.ImageShow.update()
+                break
+        if self.ImageShow.count() == 1:
+            obj = self.ImageShow.itemAt(0)
+            labI = obj.itemAt(0).widget()
+            labS = obj.itemAt(1).widget()
+            labI.setVisible(False)
+            labS.setVisible(False)
+        self.ImageCounter=0
+    def EventForLableImages(self, VLayout,event):
+        restart=True
+        while restart:
+            print(self.ImageLayout.count())
+            if self.ImageLayout.count() == 0:
+                restart=False
+            for j in range(self.ImageLayout.count()):
+                obj = self.ImageLayout.takeAt(j)
+                print(obj.widget())
+                if obj == None:
+                    restart=False
+                    break
+                if obj == VLayout:
+                    for i in self.Images:
+                        elem = obj.takeAt(1).widget()
+                        if i[0] == elem.text():
+                            self.ImageLayout.removeItem(obj)
+                            self.Images.remove(i)
+                            print(self.ImageLayout.count())
+                            break
+                    break
+                elif type(obj.widget()) == type(QtWidgets.QLabel()):
+                    self.ImageLayout.removeItem(obj)
+                    break
+                elif type(obj) == type(QtWidgets.QVBoxLayout()):
+                    restart = False
+                    break
+        self.update()
+                
+    def EventForLableStyles(self, VLayout,event):
+        lb_1 = VLayout.children[0]
+        lb_2 = VLayout.children[1]
+        self.Labels.remove(lb_1)
+        lb_1.deleteLater()
 
     def AddPictures(self):
-        self.CountStyleinImages.append([len(self.Images),len(self.Styles)])
         self.TakePic.setEnabled(True)
-
-class GoodQLabel(QtWidgets.QLabel):
-    clicked = pyqtSignal()
-    def __init__(self, parent=None, flags=Qt.WindowFlags()):
-        QtWidgets.QLabel.__init__(parent=parent, flags=flags)
-
-    def mouseDoubleClickEvent(self,event):
-        self.clicked.emit()
+        self.TakeStyle.setEnabled(False)
+        self.ShowImages()
--- CNN Style Transfer/MainCNN.py
+++ CNN Style Transfer/MainCNN.py
@@ -5,7 +5,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5.uic as uic
 
 formMain = uic.loadUiType("UI.ui")[0]
-CNN = ConvolutionNeuralNetwork()
+#CNN = ConvolutionNeuralNetwork()
 class MainWindow(QtWidgets.QMainWindow,formMain):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self,None)
@@ -29,15 +29,15 @@ class MainWindow(QtWidgets.QMainWindow,formMain):
         self.AvtoBox.setVisible(True)
 
     def AvtoModeTake(self):
-        CNN.num_steps = self.NumSteps.value()
+        #CNN.num_steps = self.NumSteps.value()
         QtWidgets.QMessageBox.information(self,'Message', f'Автоматический режим выбран, число шагов: {CNN.num_steps}')
         self.AvtoBox.setVisible(False)
         self.Mode=True
 
     def ConfirmMode(self):
         self.frame.setVisible(False)
-        if self.Mode==True:
-            CNN.Run_epoch()
+        #if self.Mode==True:
+            #CNN.Run_epoch()
         self.frame_2.setVisible(True)
 
     def TakeImages(self):
